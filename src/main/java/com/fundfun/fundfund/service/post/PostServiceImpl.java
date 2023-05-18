e4577e612476e98cfcf3df7ab07573cec081cd39
hen-ni <haein0758@naver.com>
2023-05-19 07:47:23 +0900

    fix: post

--- src/main/java/com/fundfun/fundfund/service/post/PostServiceImpl.java
+++ src/main/java/com/fundfun/fundfund/service/post/PostServiceImpl.java
@@ -3,6 +3,8 @@ package com.fundfun.fundfund.service.post;
 import com.fundfun.fundfund.domain.post.Post;
 import com.fundfun.fundfund.domain.post.StPost;
 import com.fundfun.fundfund.domain.vote.Vote;
+import com.fundfun.fundfund.dto.post.PostDto;
+import com.fundfun.fundfund.dto.post.PostRequestDto;
 import com.fundfun.fundfund.repository.post.PostRepository;
 import com.fundfun.fundfund.repository.vote.VoteRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,124 +15,127 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
-    private final PostRepository postRep;
-    private final VoteRepository voteRep;
+    private final PostRepository postRepository;
+    private final VoteRepository voteRepository;
     private final ModelMapper modelMapper;
 
-    //게시물 생성
+
+    public void createPost(PostRequestDto postRequestDTO) {
+        Post post = modelMapper.map(postRequestDTO, Post.class);
+        postRepository.save(post);
+    }
+
+    @Override
+    public List<PostDto> selectAll() {
+        List<Post> posts = postRepository.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
+        return posts.stream()
+                .map(post -> modelMapper.map(post, PostDto.class))
+                .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public List<PostDto> selectPostByKeyword(String keyword) {
+        List<Post> posts = postRepository.findByTitleContaining(keyword);
+        return posts.stream()
+                .map(post -> modelMapper.map(post, PostDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<PostDto> selectPostByUserId(UUID userId) {
+        Optional<Post> post = postRepository.findById(userId);
+        return post.map(p -> modelMapper.map(p, PostDto.class));
+    }
+
+    @Override
+    public List<PostDto> selectPostByStatus(StPost status) {
+        List<Post> posts = postRepository.findByStatusPost(status);
+        return posts.stream()
+                .map(post -> modelMapper.map(post, PostDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<PostDto> selectPostByCategory(String category) {
+        List<Post> posts = postRepository.findByCategoryPost(category);
+        return posts.stream()
+                .map(post -> modelMapper.map(post, PostDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
     public void createPost(Post post) {
-        postRep.save(post);
-    }
-
-    //전체 게시물 조회
-    public List<Post> selectAll() {
-        return postRep.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
-    }
-
-    //제목으로 게시물 조회
-    public List<Post> selectPostByKeyword(String keyword) {
-        return postRep.findByTitleContaining(keyword);
 
     }
 
-    //작성자로 게시물 조회
-    public Optional<Post> selectPostByUserId(UUID userId) {
-        return postRep.findById(userId);
+    @Override
+    public void delete(PostDto post) {
+
     }
 
-    //상태로 게시물 조회(최신순)
-    public List<Post> selectPostByStatus(StPost status) {
-        return postRep.findByStatusPost(status);
-    }
-
-    //카테고리로 게시물 조회
-    public List<Post> selectPostByCategory(String category) {
-        return postRep.findByCategoryPost(category);
-    }
-
-    //게시물 삭제
-    public void delete(Post post) {
-
-        postRep.delete(post);
+    @Override
+    public void updatePost(PostDto post) {
 
     }
 
 
-//게시물 수정
+    public void delete(UUID postId) {
+        postRepository.deleteById(postId);
+    }
 
-    public void updatePost(Post post) {
-        Post existingPost = postRep.findById(post.getId()).orElse(null);
+    public void updatePost(UUID postId, PostRequestDto postRequestDto) {
+        Post existingPost = postRepository.findById(postId).orElse(null);
         if (existingPost != null) {
-            // 변경할 필드값을 업데이트합니다.
-            existingPost.setTitle(post.getTitle());
-            existingPost.setContentPost(post.getContentPost());
-
-            // 게시물을 저장하여 업데이트합니다.
-            postRep.save(existingPost);
-
+            modelMapper.map(postRequestDto, existingPost);
+            postRepository.save(existingPost);
         } else {
-            throw new RuntimeException("검색한 게시물이 존재하지 않습니다.");
+            throw new RuntimeException("게시물이 존재하지 않습니다.");
         }
-
     }
 
-    //좋아요 순으로 게시물 정렬
-    public List<Post> getPostsOrderByLikes() {
-
-        Post post = Post.builder()
-                .title("Example Title")
-                .contentPost("Example Content")
-                .build();
-
-        return postRep.findAll(Sort.by(Sort.Direction.DESC, "like"));
-
+    @Override
+    public List<PostDto> getPostsOrderByLikes() {
+        List<Post> posts = postRepository.findAll(Sort.by(Sort.Direction.DESC, "likePost"));
+        return posts.stream()
+                .map(post -> modelMapper.map(post, PostDto.class))
+                .collect(Collectors.toList());
     }
 
-    //게시물에 좋아요 추가
     @Override
     public void addLike(UUID postId) {
-        Post post = postRep.findById(postId).orElse(null);
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("게시물이 존재하지 않습니다."));
 
-        if(post == null)
-            throw new RuntimeException("해당 게시물이 존재하지 않습니다.");
+        post.setLikePost(post.getLikePost() + 1); // 좋아요 수 증가
+        postRepository.save(post);
 
-        post.setLikePost(post.getLikePost()+1); //좋아요 + 1
-        postRep.save(post);
-        //updateStatus(post, StPost.PREPRODUCT); //좋아요 갯수 체크 및 상태 변경
-        //
+        updatePostStatus(post); // 좋아요 개수 확인 및 상태 업데이트
     }
 
-
-    public void updateStatus(Post post, StPost status){
+    @Override
+    public void updateStatus(PostDto post, StPost status) {
         post.setStatusPost(status);
         Vote vote = new Vote();
         vote.linkPost(post);
-        voteRep.save(vote);
+        voteRepository.save(vote);
         post.linkVote(vote);
-        postRep.save(post);
+        postRepository.save(post);
     }
 
-    //좋아요 100개 이상 시 상태 변경
-    public void updatePostStatus(Post post) {
-        if (post.getLikePost() >= 10) {
-            //post.setStatusPost();
+    @Override
+    public void updatePostStatus(PostDto post) {
 
-            postRep.save(post);
-        }
     }
 
-    //게시물의 좋아요 개수 조회
     @Override
     public int getLikeById(UUID postId) {
-        Post post = postRep.findById(postId).orElse(null);
-        if(post==null)
-            throw new RuntimeException("해당 게시물이 존재하지 않습니다.");
-
+        Post post = postRepository.findById(postId).orElseThrow(() -> new RuntimeException("게시물이 존재하지 않습니다."));
         return post.getLikePost();
     }
 }
--- src/test/java/com/fundfun/fundfund/service/post/PostServiceImplTest.java
+++ src/test/java/com/fundfun/fundfund/service/post/PostServiceImplTest.java
@@ -3,6 +3,7 @@ package com.fundfun.fundfund.service.post;
 import com.fundfun.fundfund.domain.post.Post;
 import com.fundfun.fundfund.domain.post.StPost;
 import com.fundfun.fundfund.domain.vote.Vote;
+import com.fundfun.fundfund.dto.post.PostDto;
 import com.fundfun.fundfund.service.vote.VoteService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,22 +34,25 @@ class PostServiceImplTest {
 //
 //            postService.createPost(post);
 //        };
-        Post post = Post.builder().title("이거어때요?!").contentPost("추천합니다").categoryPost("주식형").build();
+        Post post = Post.builder()
+                .title("이거어때요?!")
+                .contentPost("추천합니다")
+                .categoryPost("주식형").build();
         postService.createPost(post);
     }
 
     @Test
     public void 게시물조회() throws Exception {
-        List<Post> list = postService.selectAll();
-        for (Post p : list) {
+        List<PostDto> list = postService.selectAll();
+        for (PostDto p : list) {
             System.out.println("제목 : " + p.getTitle() + ", 좋아요 수 : " + p.getLikePost() + ", 상태 : " + p.getStatusPost());
         }
     }
 
     @Test
     public void 제목_게시물조회() throws Exception {
-        List<Post> list = postService.selectPostByKeyword(null);
-        for (Post p : list) System.out.println(p);
+        List<PostDto> list = postService.selectPostByKeyword(null);
+        for (PostDto p : list) System.out.println(p);
     }
 
     // @Test
@@ -59,21 +63,21 @@ class PostServiceImplTest {
 
     @Test
     public void 상태_게시물조회() throws Exception {
-        List<Post> list = postService.selectPostByStatus(StPost.EARLY_IDEA);
-        for (Post p : list) System.out.println(p.getCreatedAt() + "에 등록된 게시물의 상태 : " + p.getStatusPost());
+        List<PostDto> list = postService.selectPostByStatus(StPost.EARLY_IDEA);
+        for (PostDto p : list) System.out.println(p);
     }
 
     @Test
     public void 카테고리_게시물조회() throws Exception {
-        List<Post> list = postService.selectPostByCategory(null);
-        for (Post p : list) System.out.println(p);
+        List<PostDto> list = postService.selectPostByCategory(null);
+        for (PostDto p : list) System.out.println(p);
     }
 
     @Test
     public void 게시물삭제() throws Exception {
 
         UUID uuid = postService.selectAll().get(0).getId();
-        Optional<Post> postToDelete = postService.selectPostByUserId(uuid);
+        Optional<PostDto> postToDelete = postService.selectPostByUserId(uuid);
 
         // 게시물이 존재하는지 확인
         assertTrue(postToDelete.isPresent());
@@ -90,7 +94,7 @@ class PostServiceImplTest {
 
         UUID uuid = postService.selectAll().get(0).getId();
         // 수정할 게시물 가져오기 (수정할 게시물의 ID 또는 다른 고유 식별자 사용)
-        Optional<Post> postToUpdate = postService.selectPostByUserId(uuid);
+        Optional<PostDto> postToUpdate = postService.selectPostByUserId(uuid);
 
         // 게시물이 존재하는지 확인
         assertTrue(postToUpdate.isPresent());
@@ -105,7 +109,7 @@ class PostServiceImplTest {
             postService.updatePost(post);
 
             // 수정된 게시물 조회 및 확인
-            Optional<Post> updatedPost = postService.selectPostByUserId(uuid);
+            Optional<PostDto> updatedPost = postService.selectPostByUserId(uuid);
             assertTrue(updatedPost.isPresent());
             assertEquals("New Title", updatedPost.get().getTitle());
             assertEquals("New Content", updatedPost.get().getContentPost());
@@ -114,8 +118,8 @@ class PostServiceImplTest {
 
     @Test
     public void 좋아요정렬() throws Exception {
-        List<Post> list = postService.getPostsOrderByLikes();
-        for (Post p : list) System.out.println(p);
+        List<PostDto> list = postService.getPostsOrderByLikes();
+        for (PostDto p : list) System.out.println(p);
     }
 
     @Test
@@ -123,7 +127,7 @@ class PostServiceImplTest {
 
         UUID uuid = postService.selectAll().get(0).getId();
         // 상태 변경할 게시물 가져오기 (게시물의 ID 또는 다른 고유 식별자 사용)
-        Optional<Post> postToUpdateStatus = postService.selectPostByUserId(uuid);
+        Optional<PostDto> postToUpdateStatus = postService.selectPostByUserId(uuid);
 
         // 게시물이 존재하는지 확인
         assertTrue(postToUpdateStatus.isPresent());
@@ -132,7 +136,7 @@ class PostServiceImplTest {
         postToUpdateStatus.ifPresent(post -> {
             // 게시물의 좋아요 개수가 100개 이상인 경우 상태 변경
             if (post.getLikePost() >= 100) {
-                //post.setStatusPost("PREPRODUCT");
+                // post.setStatusPost("PREPRODUCT");
                 postService.updatePostStatus(post);
             }
         });
@@ -141,8 +145,8 @@ class PostServiceImplTest {
     @Test
     public void 상태변경및투표생성() throws Exception{
         for(int i=0; i<11; i++){
-            List<Post> list = postService.selectAll();
-            Post post = list.get(0);
+            List<PostDto> list = postService.selectAll();
+            PostDto post = list.get(0);
             UUID id = post.getId();
             if(post.getLikePost()>=5 && post.getStatusPost()==StPost.EARLY_IDEA) postService.updateStatus(post, StPost.PREPRODUCT);
             postService.addLike(id);
@@ -150,8 +154,8 @@ class PostServiceImplTest {
             System.out.println(i + "번쨰 시도) " + id + " 게시물의 좋아요 개수 : " + post.getLikePost() + ", 상태 = " + post.getStatusPost());
         }
 
-        List<Post> list = postService.selectAll();
-        Post post = list.get(0);
+        List<PostDto> list = postService.selectAll();
+        PostDto post = list.get(0);
         Vote vote = voteService.selectVoteByPostId(post.getId());
         if(vote!=null){
             System.out.println(vote.getPost().getId() + " 글에 생성된 투표의 정보 : " + vote.getStatus() + ", " + vote.getVoteStart() + ", " + vote.getVoteEnd() + ", " + vote.getId());
